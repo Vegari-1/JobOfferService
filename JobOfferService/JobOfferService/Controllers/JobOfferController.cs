bbaa10cbe60ba3d9508159884f25dce860a70983
Nikola <nikolakabasaj6@gmail.com>
2022-08-23 13:55:34 +0200

    feat: monitoring added and nuget.config placed to root repository folder (needs to be placed to solution root when running)
    fix: build error fix

--- JobOfferService/JobOfferService/Controllers/JobOfferController.cs
+++ JobOfferService/JobOfferService/Controllers/JobOfferController.cs
@@ -3,6 +3,8 @@ using JobOfferService.Dto;
 using JobOfferService.Model;
 using JobOfferService.Service.Interface;
 using Microsoft.AspNetCore.Mvc;
+using OpenTracing;
+using Prometheus;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,17 +16,27 @@ namespace JobOfferService
     {
         private readonly IJobOfferService _jobOfferService;
         private readonly IMapper _mapper;
+        private readonly ITracer _tracer;
 
-        public JobOfferController(IJobOfferService jobOfferService, IMapper mapper)
+        Counter counter = Metrics.CreateCounter("job_offer_service_counter", "job offer counter");
+
+        public JobOfferController(IJobOfferService jobOfferService, IMapper mapper, ITracer tracer)
         {
             _jobOfferService = jobOfferService;
             _mapper = mapper;
+            _tracer = tracer;
         }
 
         [HttpGet]
         [ProducesResponseType(typeof(ICollection<JobOffer>), 200)]
         public async Task<IActionResult> Filter()
         {
+            var actionName = ControllerContext.ActionDescriptor.DisplayName;
+            using var scope = _tracer.BuildSpan(actionName).StartActive(true);
+            scope.Span.Log("filter job offers");
+
+            counter.Inc();
+
             var jobOfferList = await _jobOfferService.Filter();
             var response = _mapper.Map<ICollection<JobOfferResponse>>(jobOfferList);
             return Ok(response);
@@ -36,6 +48,12 @@ namespace JobOfferService
         [ProducesResponseType(typeof(ICollection<JobOffer>), 200)]
         public async Task<IActionResult> Publish(JobOfferResponse JobOfferResponse)
         {
+            var actionName = ControllerContext.ActionDescriptor.DisplayName;
+            using var scope = _tracer.BuildSpan(actionName).StartActive(true);
+            scope.Span.Log("public job offer");
+
+            counter.Inc();
+
             var jobOffer = await _jobOfferService.PublishToQue(JobOfferResponse.Id);
             return Ok(_mapper.Map<JobOfferResponse>(jobOffer));
         }
