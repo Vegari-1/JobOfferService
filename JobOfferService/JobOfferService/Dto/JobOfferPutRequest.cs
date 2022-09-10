c7108283a8fc21c7ea679f3d6f670ac703f60d16
Dimitrije <dimitrije24000@gmail.com>
2022-09-10 20:24:49 +0200

    feat: crud operations for job offer entity

--- JobOfferService/JobOfferService/Dto/JobOfferPutRequest.cs
+++ JobOfferService/JobOfferService/Dto/JobOfferPutRequest.cs
@@ -1,12 +1,9 @@
 ﻿namespace JobOfferService.Dto;
 
-public class JobOfferResponse
+public class JobOfferPutRequest
 {
-    public string Id { get; set; }
-    public string GlobalId { get; set; }
     public string PositionName { get; set; }
     public string Description { get; set; }
     public IList<string> Qualifications { get; set; }
     public string CompanyLink { get; set; }
-    public ProfileResponse Profile { get; set; }
 }
--- JobOfferService/JobOfferService/Profiles/JobOfferProfile.cs
+++ JobOfferService/JobOfferService/Profiles/JobOfferProfile.cs
@@ -8,8 +8,9 @@ public class JobOfferProfile : AutoMapper.Profile
     public JobOfferProfile()
     {
         // Source -> Target
-        CreateMap<JobOffer, JobOfferResponse>();
-        CreateMap<JobOfferRequest, JobOffer>();
+        CreateMap<JobOffer, JobOfferResponse>().ForMember(dest => dest.Profile, src => src.MapFrom(s => s.CreatedBy));
+        CreateMap<JobOfferPostRequest, JobOffer>().ForMember(dest => dest.CreatedBy, src => src.MapFrom(s => s.Profile));
+        CreateMap<JobOfferPutRequest, JobOffer>();
     }
 }
 
